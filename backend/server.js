25bf9709ba57d4f7dae3a43f532e86443b06a94d
IloveGreeny <greenxd23@gmail.com>
2022-09-17 19:33:34 +0400

    fourth commit

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,10 @@ const express = require("express");
 
 const app = express()
 
-
-
-
-
-
-
-
+app.get("/",((req,res)=> {
+    res.status(200);
+    res.json({country : "Armenia"});
+}))
 
 app.listen(5000, ()=>{
     console.log("Running on Port 5000");

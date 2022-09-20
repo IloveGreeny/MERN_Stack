f40588b9d86997eaecef52c6cec0115dacca1e10
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-09-20 22:15:22 +0400

    Update countryControllers.js

--- backend/controllers/countryControllers.js
+++ backend/controllers/countryControllers.js
@@ -1,15 +1,7 @@
 const Countries = require("../models/countryModel");
 const mongoose = require("mongoose");
 
-const createCountry = async (req,res) => {
-    const {title, capital, weather} = req.body;
-    try {
-        const country = await Countries.create({title,capital,weather})
-        res.status(200).json(country)
-    }catch (err) {
-        res.status(400).json({error : err.message})
-    }
-};
+
 
 const getAllCountries = async (req,res) => {
     const countries = await Countries.find({}).sort({createdAt : -1});

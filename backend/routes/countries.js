7af00c2c64ad7e7546e4327a30c3f34f090053fb
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-09-20 22:16:45 +0400

    Update countries.js

--- backend/routes/countries.js
+++ backend/routes/countries.js
@@ -2,25 +2,9 @@ const express = require("express");
 const {createCountry,getAllCountries, getOneCountry} = require("../controllers/countryControllers");
 const router = express.Router();
 
-router.get("/",(req,res)=> {
-    res.status(200);
-    res.json({message : "Get All Countries"});
-});
+router.get("/",getAllCountries);
 
-router.get("/:id",(req,res)=> {
-    res.status(200);
-    res.json({message : "Get One Country"});
-});
 
-router.post("/",async (req,res)=> {
-    const {title, capital, weather} = req.body;
-    try {
-        const country = await Countries.create({title,capital,weather})
-        res.status(200).json(country)
-    }catch (err) {
-        res.status(400).json({error : err.message})
-    }
-});
 
 router.delete("/:id",(req,res)=> {
     res.status(200);

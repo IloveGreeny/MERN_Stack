703339daf15f003559d70dadc3b7b32ad4c47112
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-09-24 23:04:34 +0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
-import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
 import Home from "./Pages/Home/Home"
-import {Navbar} from "./Componets/Navbar/Navbar";
+import { Navbar } from "./Componets/Navbar/Navbar";
 
 export default function App() {
   return (

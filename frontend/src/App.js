7d004613cb0663e92bf32e7d1e8c14f65a6f8cb7
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-10-01 23:02:50 +0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,1 @@
-import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
-import Home from "./Pages/Home/Home"
-import {Navbar} from "./Componets/Navbar/Navbar";
-
-export default function App() {
-  return (
-      <Router>
-          <Navbar/>
-            <Routes>
-              <Route
-              path="/"
-              element={<Home/>}
-              />
-            </Routes>
-      </Router>
-  );
-}
 
